811bb2f5ec178dc52c44c0fe2692d9f4c96e870b
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2016-10-29 17:16:00 +0200

    stashíng estimote playground

--- app/src/main/java/uk/co/droidcon/hack/bstf/BstfApplication.kt
+++ app/src/main/java/uk/co/droidcon/hack/bstf/BstfApplication.kt
@@ -3,6 +3,7 @@ package uk.co.droidcon.hack.bstf
 import android.app.Application
 import android.content.Intent
 import android.content.IntentFilter
+import com.estimote.sdk.EstimoteSDK
 
 import timber.log.Timber
 import uk.co.droidcon.hack.bstf.reload.battery.BatteryStateReceiver
@@ -18,6 +19,9 @@ class BstfApplication : Application() {
         batteryFilter.addAction(Intent.ACTION_BATTERY_LOW)
         batteryFilter.addAction(Intent.ACTION_BATTERY_CHANGED)
         registerReceiver(BatteryStateReceiver(), batteryFilter)
+
+        EstimoteSDK.initialize(applicationContext, "lavong-soysavanh-gmail-com-os5", "397493679d78b78462cd60dc21083cda")
+        EstimoteSDK.enableDebugLogging(true)
     }
 
 }
--- app/src/main/java/uk/co/droidcon/hack/bstf/MainActivity.java
+++ app/src/main/java/uk/co/droidcon/hack/bstf/MainActivity.java
@@ -3,7 +3,16 @@ package uk.co.droidcon.hack.bstf;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+
+import java.util.List;
+import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,10 +24,16 @@ import uk.co.droidcon.hack.bstf.scan.ScanControllerImpl;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.text_view) TextView textView;
+    private static final String TAG = "abc";
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.text_view)
+    TextView textView;
 
     private ScanController scanController;
+    private BeaconManager beaconManager;
+
+    private Region region = new Region("monitored region", UUID.fromString("BBE1114A-0CDB-433D-B0C3-97AD44F9F639"), 1001, 1001);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,53 @@ public class MainActivity extends AppCompatActivity {
                         textView.setText(s);
                     }
                 });
+
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    Log.d(TAG, "onBeaconsDiscovered() called with: region = [" + region + "], list = [" + list + "]");
+                    Beacon nearestBeacon = list.get(0);
+                    Log.d(TAG, "nearest beacon: " + nearestBeacon);
+                }
+            }
+        });
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+                Log.d(TAG, "onEnteredRegion() called with: region = [" + region + "], list = [" + list + "]");
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+                Log.d(TAG, "onExitedRegion() called with: region = [" + region + "]");
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+        Log.d(TAG, "connect");
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                Log.d(TAG, "onServiceReady()");
+                beaconManager.startMonitoring(region);
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "disconnect");
+        beaconManager.stopMonitoring(region);
+        beaconManager.stopRanging(region);
+        beaconManager.disconnect();
+        super.onPause();
     }
 
     @Override
