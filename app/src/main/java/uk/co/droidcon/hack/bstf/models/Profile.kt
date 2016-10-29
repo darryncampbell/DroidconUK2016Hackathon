935aca377d0d3a0fefe24f05408bca0e9f96d7f3
Jordy Langen <jordylangen@gmail.com>
2016-10-29 18:06:02 +0200

    added zebra avatar

--- app/src/main/java/uk/co/droidcon/hack/bstf/models/Profile.kt
+++ app/src/main/java/uk/co/droidcon/hack/bstf/models/Profile.kt
@@ -6,6 +6,7 @@ enum class Profile(val id: String, val avatarId: Int) {
     SPIDERMAN("Spiderman", R.drawable.avatar_spiderman),
     GOKU("Goku", R.drawable.avatar_goku),
     DOGE("Doge", R.drawable.avatar_doge),
+    ZEBRA("Zebra", R.drawable.avatar_zebra),
     TERMINATOR("Terminator", R.drawable.avatar_terminator);
 
     companion object {
@@ -16,6 +17,7 @@ enum class Profile(val id: String, val avatarId: Int) {
                 GOKU.id -> GOKU
                 DOGE.id -> DOGE
                 TERMINATOR.id -> TERMINATOR
+                ZEBRA.id -> ZEBRA
                 else -> throw IllegalArgumentException("No profile with id " + id)
             }
         }
