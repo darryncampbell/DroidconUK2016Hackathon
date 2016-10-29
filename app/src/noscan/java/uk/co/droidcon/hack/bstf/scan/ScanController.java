dbf74bb57ad80efe099df283df63b2e516143415
Christian Langer <christian@smartpioneers.com>
2016-10-29 15:08:46 +0200

    move all scan specifics to scan flavor

---
+++ app/src/noscan/java/uk/co/droidcon/hack/bstf/scan/ScanController.java
@@ -0,0 +1,13 @@
+package uk.co.droidcon.hack.bstf.scan;
+
+import android.content.Context;
+
+public class ScanController {
+
+    public void onCreate(Context context) {
+    }
+
+    public void onDestroy() {
+    }
+
+}
